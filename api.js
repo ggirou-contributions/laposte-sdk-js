052eba366e28ede99ab6f36a586dab167a40c426
Olivier Penhoat <openhoat@gmail.com>
2015-03-17 11:18:16 +0100

    Updates

---
+++ api.js
@@ -0,0 +1,19 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Digiposte",
+        "HttpError",
+        "LaPoste"
+    ],
+    "modules": [
+        "laPosteSdk"
+    ],
+    "allModules": [
+        {
+            "displayName": "laPosteSdk",
+            "name": "laPosteSdk",
+            "description": "Provides the base class of the La Poste Open API SDK."
+        }
+    ]
+} };
+});
